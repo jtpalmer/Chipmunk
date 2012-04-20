b0688d473287044ca350871f6561e9e27a2f6080
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2012-04-20 19:15:44 -0400

    Add module implementation

--- lib/Chipmunk.pm
+++ lib/Chipmunk.pm
@@ -1,5 +1,13 @@
+package Chipmunk;
+use 5.008001;
 use strict;
 use warnings;
-package Chipmunk;
+use Alien::Chipmunk;
+
+our $VERSION = '0.000001';
+$VERSION = eval $VERSION;
+
+require XSLoader;
+XSLoader::load('Chipmunk', $VERSION);
 
 1;

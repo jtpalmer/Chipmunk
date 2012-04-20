3b6958a2aa7ab875ac930fbbc18dad13ca614544
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2012-04-20 19:15:59 -0400

    Add documentation

--- lib/Chipmunk.pm
+++ lib/Chipmunk.pm
@@ -11,3 +11,32 @@ require XSLoader;
 XSLoader::load('Chipmunk', $VERSION);
 
 1;
+
+__END__
+
+=head1 NAME
+
+Chipmunk - Perl bindings for the Chipmunk Physics library
+
+=head1 SYNOPSIS
+
+    use Chipmunk;
+
+=head1 DESCRIPTION
+
+Perl bindings for the Chipmunk Physics library.
+
+=head1 AUTHOR
+
+Jeffrey T. Palmer, E<lt>jtpalmer@cpan.org<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2012 by Jeffrey Palmer
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.14.2 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
+

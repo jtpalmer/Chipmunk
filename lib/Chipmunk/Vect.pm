d3213869b200114a3c629426cd61c0fe1fa83235
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2013-08-03 10:51:10 -0400

    Add some docs

--- lib/Chipmunk/Vect.pm
+++ lib/Chipmunk/Vect.pm
@@ -1,6 +1,6 @@
 package Chipmunk::Vect;
 
-# ABSTRACT: 2D vector type along with a handy 2D vector math lib
+# ABSTRACT: Handy 2D vector math lib
 
 use strict;
 use warnings;
@@ -55,9 +55,73 @@ __END__
 
 =head1 SYNOPSIS
 
+    use Chipmunk::Vect qw(:all);
+
+    my $v1 = [ 1.1, 2.2 ];
+    my $v2 = [ 3.3, 4.4 ];
+
+    my $sum  = cpvadd( $v1, $v2 );
+    my $diff = cpvsub( $v1, $v2 );
+
 =head1 DESCRIPTION
 
+Vector functions.
+
 =head1 FUNCTIONS
+
+=head2 cpvslerp
+
+=head2 cpvslerpconst
+
+=head2 cpvstr
+
+=head2 cpveql
+
+=head2 cpvadd
+
+=head2 cpvsub
+
+=head2 cpvneg
+
+=head2 cpvmult
+
+=head2 cpvdot
+
+=head2 cpvcross
+
+=head2 cpvperp
+
+=head2 cpvrperp
+
+=head2 cpvproject
+
+=head2 cpvforangle
+
+=head2 cpvtoangle
+
+=head2 cpvrotate
+
+=head2 cpvunrotate
+
+=head2 cpvlengthsq
+
+=head2 cpvlength
+
+=head2 cpvlerp
+
+=head2 cpvnormalize
+
+=head2 cpvnormalize_safe
+
+=head2 cpvclamp
+
+=head2 cpvlerpconst
+
+=head2 cpvdist
+
+=head2 cpvdistsq
+
+=head2 cpvnear
 
 =cut
 

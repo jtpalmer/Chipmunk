ac0580145a4c381b7944d617e336f16b041ba989
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2013-08-18 12:25:21 -0400

    Add docs boilerplate

--- lib/Chipmunk/Shape.pm
+++ lib/Chipmunk/Shape.pm
@@ -17,5 +17,49 @@ __END__
 
 =head1 DESCRIPTION
 
+=head1 METHODS
+
+=head2 cache_bb
+
+=head2 update
+
+=head2 point_query
+
+=head2 get_space
+
+=head2 get_body
+
+=head2 set_body
+
+=head2 get_bb
+
+=head2 get_sensor
+
+=head2 set_sensor
+
+=head2 get_elasticity
+
+=head2 set_elasticity
+
+=head2 get_friction
+
+=head2 set_friction
+
+=head2 get_surface_velocity
+
+=head2 set_surface_velocity
+
+=head2 get_collision_type
+
+=head2 set_collision_type
+
+=head2 get_group
+
+=head2 set_group
+
+=head2 get_layers
+
+=head2 set_layers
+
 =cut
 

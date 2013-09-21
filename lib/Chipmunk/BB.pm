5b41a784c7cdb3855a5ed87a5e13bf36a4ad460a
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2013-09-21 11:03:37 -0400

    Add doc boilerplate

--- lib/Chipmunk/BB.pm
+++ lib/Chipmunk/BB.pm
@@ -17,5 +17,33 @@ __END__
 
 =head1 DESCRIPTION
 
+=head1 METHODS
+
+=head2 new
+
+=head2 new_for_circle
+
+=head2 intersects
+
+=head2 contains_bb
+
+=head2 contains_vect
+
+=head2 merge
+
+=head2 expand
+
+=head2 area
+
+=head2 merged_area
+
+=head2 segment_query
+
+=head2 intersects_segment
+
+=head2 clamp_vect
+
+=head2 wrap_vect
+
 =cut
 

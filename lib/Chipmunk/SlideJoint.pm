f782bb7e88a5ba67d6c5a8b817f831e7713c7c3c
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2013-10-24 17:49:12 -0400

    Add doc boilerplate

--- lib/Chipmunk/SlideJoint.pm
+++ lib/Chipmunk/SlideJoint.pm
@@ -19,5 +19,25 @@ __END__
 
 =head1 DESCRIPTION
 
+=head1 METHODS
+
+=head2 new
+
+=head2 get_anchr1
+
+=head2 set_anchr1
+
+=head2 get_anchr2
+
+=head2 set_anchr2
+
+=head2 get_min
+
+=head2 set_min
+
+=head2 get_max
+
+=head2 set_max
+
 =cut
 

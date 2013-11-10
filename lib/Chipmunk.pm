b01ad4d113c848fc68a7b7c4629222df88cace2e
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2013-11-10 08:19:43 -0500

    Add more docs

--- lib/Chipmunk.pm
+++ lib/Chipmunk.pm
@@ -38,9 +38,45 @@ Perl bindings for the Chipmunk Physics library.
 
 =head1 METHODS
 
-=head2 segment_to_segment_collisions
+=head2 enable_segment_to_segment_collisions
+
+=over 4
+
+=item Arguments: none
+
+=item Return value: not defined
+
+=back
+
+Chipmunk didn't support segment/segment collisions until v6.1.2. For
+compatibility reasons, you must explicitly and globally enable them.
+
+    enable_segment_to_segment_collisions();
 
 =head2 reset_shape_id_counter
+
+=over 4
+
+=item Arguments: none
+
+=item Return value: not defined
+
+=back
+
+Chipmunk keeps a counter so that every new shape is given a unique hash
+value to be used in the spatial index. Because this affects the order in
+which the collisions are found and handled, you can reset the shape
+counter every time you populate a space with new shapes. If you don't,
+there might be (very) slight differences in the simulation.
+
+    reset_shape_id_counter();
+
+=head1 NOTICE
+
+The documentation is taken from
+L<Chipmunk2D|http://chipmunk-physics.net/release/ChipmunkLatest-Docs/>
+
+Copyright (c) 2007-2013 Scott Lembcke and Howling Moon Software
 
 =head1 SEE ALSO
 

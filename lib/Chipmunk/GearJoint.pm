530d8a0b6b84cd4807f2a8143876a48b10e2d89b
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2013-11-14 17:45:05 -0500

    Add docs

--- lib/Chipmunk/GearJoint.pm
+++ lib/Chipmunk/GearJoint.pm
@@ -17,6 +17,9 @@ __END__
 
 =head1 SYNOPSIS
 
+   $joint = Chipmunk::GearJoint->new( $body_a, $body_b, $phase, $ratio );
+   $space->add_constraint($joint);
+
 =head1 DESCRIPTION
 
 Keeps the angular velocity ratio of a pair of bodies constant. C<$ratio>
@@ -28,13 +31,63 @@ is the initial angular offset of the two bodies.
 
 =head2 new
 
+=over 4
+
+=item Arguments: L<$body_a|Chipmunk::Body>, L<$body_b|Chipmunk::Body>, $phase, $ratio
+
+=item Return Value: $joint
+
+=back
+
+   $joint = Chipmunk::GearJoint->new( $body_a, $body_b, $phase, $ratio );
+
 =head2 get_phase
+
+=over 4
+
+=item Arguments: none
+
+=item Return Value: $phase
+
+=back
+
+    $phase = $joint->get_phase();
 
 =head2 set_phase
 
+=over 4
+
+=item Arguments: $phase
+
+=item Return Value: not defined
+
+=back
+
+    $joint->set_phase($phase);
+
 =head2 get_ratio
 
+=over 4
+
+=item Arguments: none
+
+=item Return Value: $ratio
+
+=back
+
+    $ratio = $joint->get_ratio();
+
 =head2 set_ratio
+
+=over 4
+
+=item Arguments: $ratio
+
+=item Return Value: not defined
+
+=back
+
+    $joint->set_ratio($ratio);
 
 =head1 SEE ALSO
 

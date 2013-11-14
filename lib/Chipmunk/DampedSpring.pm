41140cf6b3cc05e582ecd9eaccd262e90dc45237
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2013-11-13 19:31:05 -0500

    Add docs

--- lib/Chipmunk/DampedSpring.pm
+++ lib/Chipmunk/DampedSpring.pm
@@ -17,6 +17,9 @@ __END__
 
 =head1 SYNOPSIS
 
+    $spring = Chipmunk::DampedSpring->new( $body_a, $body_b, $anchr1,
+            $anchr2, $rest_length, $stiffness, $damping );
+
 =head1 DESCRIPTION
 
 Defined much like a slide joint. C<$rest_length> is the distance the
@@ -27,6 +30,17 @@ C<$damping> is how soft to make the damping of the spring.
 =head1 METHODS
 
 =head2 new
+
+=over 4
+
+=item Arguments: L<$body_a|Chipmunk::Body>, L<$body_a|Chipmunk::Body>, L<$anchr1|Chipmunk::Vect>, L<$anchr2|Chipmunk::Vect>, $rest_length, $stiffness, $damping 
+
+=item Return Value: $spring
+
+=back
+
+    $spring = Chipmunk::DampedSpring->new( $body_a, $body_b, $anchr1,
+            $anchr2, $rest_length, $stiffness, $damping );
 
 =head1 SEE ALSO
 

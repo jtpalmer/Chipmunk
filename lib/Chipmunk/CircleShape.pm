79d84a9a33564c319e43de85de5287d35cea1769
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2013-11-15 17:26:24 -0500

    Add docs

--- lib/Chipmunk/CircleShape.pm
+++ lib/Chipmunk/CircleShape.pm
@@ -17,13 +17,47 @@ __END__
 
 =head1 SYNOPSIS
 
+    $circle = Chipmunk::CircleShape->new( $body, $radius, $offset );
+
 =head1 DESCRIPTION
 
 =head1 METHODS
 
+=head2 new
+
+=over 4
+
+=item Arguments: L<$body|Chipmunk::Body>, $radius, L<$offset|Chipmunk::Vect>
+
+=item Return Value: $circle
+
+=back
+
+    $circle = Chipmunk::CircleShape->new( $body, $radius, $offset );
+
 =head2 get_offset
 
+=over 4
+
+=item Arguments: none
+
+=item Return Value: L<$offset|Chipmunk::Vect>
+
+=back
+
+    $offset = $circle->get_offset();
+
 =head2 get_radius
+
+=over 4
+
+=item Arguments: none
+
+=item Return Value: $radius
+
+=back
+
+    $radius = $circle->get_radius();
 
 =head1 SEE ALSO
 

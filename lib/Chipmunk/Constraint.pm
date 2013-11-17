f4e61343a10bc953281ebeaa18c2f93433fc60d3
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2013-11-17 07:03:13 -0500

    Add more docs

--- lib/Chipmunk/Constraint.pm
+++ lib/Chipmunk/Constraint.pm
@@ -26,37 +26,143 @@ your body, or they can be more abstract like the gear joint or motors.
 
 =head2 activate_bodies
 
+=over 4
+
+=item Arguments: none
+
+=item Return Value: not defined
+
+=back
+
     $constraint->activate_bodies();
 
 =head2 get_space
 
-    my $space = $constraint->get_space();
+=over 4
+
+=item Arguments: none
+
+=item Return Value: L<$space|Chipmunk::Space>
+
+=back
+
+    $space = $constraint->get_space();
 
 =head2 get_a
 
-    my $body_a = $constraint->get_a();
+=over 4
+
+=item Arguments: none
+
+=item Return Value: L<$body|Chipmunk::Body>
+
+=back
+
+    $body_a = $constraint->get_a();
 
 =head2 get_b
 
-    my $body_b = $constraint->get_b();
+=over 4
+
+=item Arguments: none
+
+=item Return Value: L<$body|Chipmunk::Body>
+
+=back
+
+    $body_b = $constraint->get_b();
 
 =head2 get_max_force
 
+=over 4
+
+=item Arguments: none
+
+=item Return Value: $max_force
+
+=back
+
+    $max_force = $constraint->get_max_force();
+
 =head2 set_max_force
+
+=over 4
+
+=item Arguments: $max_force
+
+=item Return Value: not defined
+
+=back
 
 =head2 get_error_bias
 
+=over 4
+
+=item Arguments: none
+
+=item Return Value: $error_bias
+
+=back
+
 =head2 set_error_bias
 
-=head2 get_max_bias
+=over 4
+
+=item Arguments: $error_bias
+
+=item Return Value: not defined
+
+=back
 
 =head2 get_max_bias
+
+=over 4
+
+=item Arguments: none
+
+=item Return Value: $max_bias
+
+=back
+
+=head2 set_max_bias
+
+=over 4
+
+=item Arguments: $max_bias
+
+=item Return Value: not defined
+
+=back
 
 =head2 get_user_data
 
+=over 4
+
+=item Arguments: none
+
+=item Return Value: $user_data
+
+=back
+
 =head2 set_user_data
 
+=over 4
+
+=item Arguments: $user_data
+
+=item Return Value: not defined
+
+=back
+
 =head2 get_impulse
+
+=over 4
+
+=item Arguments: none
+
+=item Return Value: $impulse
+
+=back
 
 =head1 SEE ALSO
 

71d1fbed0e1f70af2c060509628ddef45b261a25
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2013-11-19 13:20:38 -0500

    Add docs

--- lib/Chipmunk/GrooveJoint.pm
+++ lib/Chipmunk/GrooveJoint.pm
@@ -17,6 +17,10 @@ __END__
 
 =head1 SYNOPSIS
 
+    $joint = Chipmunk::GrooveJoint->new( $body_a, $body_b, $groove_a,
+        $groove_b, $anchr2 );
+    $space->add_constraint($joint);
+
 =head1 DESCRIPTION
 
 The groove goes from C<$groove_a> to C<$groove_b> on body C<$a>, and the
@@ -27,17 +31,73 @@ local.
 
 =head2 new
 
+=over 4
+
+=item Arguments: L<$body_a|Chipmunk::Body>, L<$body_b|Chipmunk::Body>, L<$groove_a|Chipmunk::Vect>, L<$groove_b|Chipmunk::Vect>, L<$anchr2|Chipmunk::Vect>
+
+=item Return Value: $joint
+
+=back
+
 =head2 get_groove_a
+
+=over 4
+
+=item Arguments: none
+
+=item Return Value: L<$groove_a|Chipmunk::Vect>
+
+=back
 
 =head2 set_groove_a
 
+=over 4
+
+=item Arguments: L<$groove_a|Chipmunk::Vect>
+
+=item Return Value: not defined
+
+=back
+
 =head2 get_groove_b
+
+=over 4
+
+=item Arguments: none
+
+=item Return Value: L<$groove_b|Chipmunk::Vect>
+
+=back
 
 =head2 set_groove_b
 
+=over 4
+
+=item Arguments: L<$groove_b|Chipmunk::Vect>
+
+=item Return Value: not defined
+
+=back
+
 =head2 get_anchr2
 
+=over 4
+
+=item Arguments: none
+
+=item Return Value: L<$anchr2|Chipmunk::Vect>
+
+=back
+
 =head2 set_anchr2
+
+=over 4
+
+=item Arguments: L<$anchr2|Chipmunk::Vect>
+
+=item Return Value: not defined
+
+=back
 
 =head1 SEE ALSO
 

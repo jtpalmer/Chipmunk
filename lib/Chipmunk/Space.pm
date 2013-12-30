3266ff4fb49ffaf225114f52863352026f669163
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2013-12-30 18:21:17 -0500

    Add doc boilerplate

--- lib/Chipmunk/Space.pm
+++ lib/Chipmunk/Space.pm
@@ -28,61 +28,293 @@ Physics.
 
 =head2 new
 
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
+
 =head2 get_iterations
+
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
 
 =head2 set_iterations
 
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
+
 =head2 get_gravity
+
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
 
 =head2 set_gravity
 
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
+
 =head2 get_damping
+
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
 
 =head2 set_damping
 
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
+
 =head2 get_idle_speed_threshold
+
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
 
 =head2 set_idle_speed_threshold
 
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
+
 =head2 get_sleep_time_threshold
+
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
 
 =head2 set_sleep_time_threshold
 
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
+
 =head2 get_collision_slop
+
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
 
 =head2 set_collision_slop
 
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
+
 =head2 get_collision_bias
+
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
 
 =head2 set_collision_bias
 
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
+
 =head2 get_collision_persistence
+
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
 
 =head2 set_collision_persistence
 
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
+
 =head2 get_enable_contact_graph
+
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
 
 =head2 set_enable_contact_graph
 
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
+
 =head2 get_user_data
+
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
 
 =head2 set_user_data
 
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
+
 =head2 get_static_body
+
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
 
 =head2 get_current_time_step
 
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
+
 =head2 is_locked
+
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
 
 =head2 set_default_collision_handler
 
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
+
 =head2 add_collision_handler
+
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
 
 =head2 remove_collision_handler
 
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
+
 =head2 add_shape
 
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
+
 =head2 add_static_shape
+
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
 
 B<DEPRECATED>
 
@@ -94,11 +326,43 @@ anymore.
 
 =head2 add_body
 
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
+
 =head2 add_constraint
+
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
 
 =head2 remove_shape
 
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
+
 =head2 remove_static_shape
+
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
 
 B<DEPRECATED>
 
@@ -108,15 +372,63 @@ See L</"add_static_shape">
 
 =head2 remove_body
 
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
+
 =head2 remove_constraint
+
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
 
 =head2 contains_shape
 
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
+
 =head2 contains_body
+
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
 
 =head2 contains_constraint
 
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
+
 =head2 convert_body_to_static
+
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
 
 Convert a body to a static one. It's mass and moment will be set to
 infinity and it's velocity to 0.  The old mass, moment and velocity are
@@ -130,6 +442,14 @@ from another callback or iterator.
 
 =head2 convert_body_to_dynamic
 
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
+
 Convert a static to a dynamic one. This does not call L</"add_body"> to
 make the body active, so you will need to do that explicitly if you need
 it. Also, because it modifies collision detection data structures, you
@@ -140,37 +460,173 @@ callback or iterator.
 
 =head2 add_post_step_callback
 
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
+
 =head2 point_query
+
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
 
 =head2 point_query_first
 
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
+
 =head2 nearest_point_query
+
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
 
 =head2 segment_query
 
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
+
 =head2 segment_query_first
+
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
 
 =head2 bb_query
 
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
+
 =head2 shape_query
+
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
 
 =head2 active_shapes_touching_shape
 
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
+
 =head2 each_body
+
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
 
 =head2 each_shape
 
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
+
 =head2 each_constraint
+
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
 
 =head2 reindex_static
 
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
+
 =head2 reindex_shape
+
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
 
 =head2 reindex_shapes_for_body
 
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
+
 =head2 use_spatial_hash
 
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
+
 =head2 step
+
+=over 4
+
+=item Arguments:
+
+=item Return Value:
+
+=back
 
 =head1 SEE ALSO
 
